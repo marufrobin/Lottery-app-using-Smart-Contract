4e3fc4c5660cf8a810f41ab5efa59dca058e4a27
marufrobin <maruf.robin@gmail.com>
2022-02-04 16:05:54 +0600

    Modifier for solidity...

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -14,7 +14,9 @@ contract Lottery {
     function random() public view returns (uint256) {
         // return
         uint256 initialNumber;
-        return uint256(keccak256(abi.encodePacked(initialNumber++))) % players.length;
+        return
+            uint256(keccak256(abi.encodePacked(initialNumber++))) %
+            players.length;
         // uint256(
         //     keccak256(
         //         abi.encodePacked(
@@ -28,5 +30,18 @@ contract Lottery {
 
     function playersList() public view returns (address payable[] memory) {
         return players;
+    }
+
+    function winner() public {
+        uint256 winnerIndex = random();
+        address payable winner = players[winnerIndex];
+        winner.transfer(address(this).balance);
+        players = new address payable[](0);
+    }
+
+    modifier onlyForOwner() {
+        require(msg.sender == owner);
+        _; ///Function body place holder rest of
+        ///the code will exucate by replaceing "_;" here
     }
 }
